8a03b3d56f1f822a4dd4201cb8e2188a036e2182
Cterminis <carlos.termini@mercadolibre.cl>
2021-09-24 15:55:08 -0300

    rotacion bloqueada

--- app/src/main/java/com/example/notbored/ui/main/fragments/Detail.kt
+++ app/src/main/java/com/example/notbored/ui/main/fragments/Detail.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.*
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -12,7 +12,6 @@ import androidx.navigation.fragment.findNavController
 import com.example.notbored.R
 import com.example.notbored.databinding.FragmentDetailBinding
 import com.example.notbored.ui.main.MainViewModel
-import kotlin.random.Random
 
 
 class Detail : Fragment(R.layout.fragment_detail) {
@@ -84,7 +83,7 @@ class Detail : Fragment(R.layout.fragment_detail) {
                     tvDetalleTitulo.isVisible = !res
                     linearLayout2.isVisible = !res
                     errShowMessage.observe(viewLifecycleOwner) {
-                        Toast.makeText(context, "$it.error", Toast.LENGTH_SHORT).show()
+                        Toast.makeText(context, "${it.error}", Toast.LENGTH_SHORT).show()
                     }
                 }
 
